1627191770b853ee247a52f909b776a360e760db
johnathanwastaken <danielortiz2001@hotmail.com>
2022-08-04 19:41:38 -0700

    adding the about me page and adding code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Bar from './components/navbar'; 
+import About from  './components/aboutme';
 
 function App() {
   return (
     <div className="App">
       <Bar/>
+      <About />
     </div>
   );
 }
