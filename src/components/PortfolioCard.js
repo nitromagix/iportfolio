9a5531f13fa91830c28c4599d1232271f4a436d3
Martin <nitromagix@gmail.com>
2022-08-07 20:18:56 -0700

    style change

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -4,12 +4,21 @@ import TempProjectImage from "../test_project.jpg";
 
 function PortfolioCard(props) {
   const data = props.data;
-
+console.log(data.imageUrl)
   return (
     <div className="card">
       <h5>{data.name}</h5>
-      <img src={TempProjectImage} alt={data.name} />
-      <p><strong>Tech</strong>: {data.tech}</p>
+      <img
+        src={
+          data.imageUrl && (data.imageUrl !== "")
+            ? data.imageUrl
+            : TempProjectImage
+        }
+        alt={data.name}
+      />
+      <p>
+        <strong>Tech</strong>: {data.tech}
+      </p>
       <p>{data.description}</p>
     </div>
   );
