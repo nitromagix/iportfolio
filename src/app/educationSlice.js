247010d95f2d8d72467704ff5316b167cc85957c
johnathanwastaken <danielortiz2001@hotmail.com>
2022-08-08 20:18:46 -0700

    updating education slice

---
+++ src/app/educationSlice.js
@@ -0,0 +1,28 @@
+//
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  educationData: {
+    education: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+    experience: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+  },
+};
+
+export const educationSlice = createSlice({
+  name: "eduaction",
+  initialState,
+  reducers: {   
+    setEducationData: (state, action) => {
+      state.educationData = {
+        ...action.payload,
+      };
+    },
+  },
+});
+
+export const { setEducationData } = educationSlice.actions;
+
+export const getEducationData = (state) => state.education.educationData;
+
+export default educationSlice.reducer;
